390969d184b39c7486536f5991cabc75a06603e3
Mirko Pica <picamirko02@gmail.com>
2022-11-08 15:18:55 +0100

    Searchbar Now Working

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
-from flask import Flask, url_for
+from flask import Flask, request, url_for, render_template
 from markupsafe import escape
 import animeworld as aw
-from flask import render_template
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
 
-@app.get('/login')
-def form_get():
-    return render_template('form.html')
-
-@app.post('/login')
-def form_post():
-    return do_the_login()
-
-@app.route("/anime/<anime>")
-def anime(anime):
-    res = aw.find(f"{escape(anime)}")
-    return render_template('anime.html', allAnime=res)
-
-    
-
+@app.route('/', methods=["GET", "POST"])
+def default():
+    if request.method == "POST":
+        anime_name = request.form.get("anime_name")
+        res = aw.find(f"{escape(anime_name)}")
+        return render_template(f"anime.html", allAnime=res)
+    return render_template("index.html")
