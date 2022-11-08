f79f41cb60db91ecf259e8d89af0fd00f25ec30b
Mirko Pica <picamirko02@gmail.com>
2022-11-08 15:41:21 +0100

    Graphic Improvements

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from markupsafe import escape
 import animeworld as aw
 app = Flask(__name__)
 
+#Comando per eseguire il tutto $ flask --app main.py --debug run
 
 @app.route('/', methods=["GET", "POST"])
 def default():
     if request.method == "POST":
-        anime_name = request.form.get("anime_name")
-        res = aw.find(f"{escape(anime_name)}")
-        return render_template(f"anime.html", allAnime=res)
+        search_text = request.form.get("anime_name")
+        res = aw.find(f"{escape(search_text)}")
+        return render_template(f"anime.html", allAnime=res, search_text=search_text)
     return render_template("index.html")
